14b5050e1bdb7f1518b4a319c64305e746ec44ab
janakbista <bistajanak303@gmail.com>
2022-09-08 17:51:56 +0545

    Recycler View data added to the app and Icon added to the project

--- app/src/main/java/com/example/todoapplication/HomeActivity.kt
+++ app/src/main/java/com/example/todoapplication/HomeActivity.kt
@@ -2,7 +2,10 @@ package com.example.todoapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapplication.adapter.RvAdapter
+import java.util.*
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,9 +13,25 @@ class HomeActivity : AppCompatActivity() {
         setContentView(R.layout.activity_home)
         setSupportActionBar(findViewById(R.id.action_bar));
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.action_bar) ;
-//        setSupportActionBar(toolbar);
-//        getSupportActionBar().setDisplayShowTitleEnabled(true);
-//        getSupportActionBar().setDisplayHomeAsUpEnabled(false);
+        val recyclerview = findViewById<RecyclerView>(R.id.rvList);
+        recyclerview.layoutManager = LinearLayoutManager(this);
+        // ArrayList of class ItemsViewModel
+        val data = ArrayList<String>()
+          data.add("Hello");
+          data.add("Hamropatro");
+          data.add("Nepal");
+          data.add("Usa");
+        data.add("Hello");
+        data.add("Hamropatro");
+        data.add("Nepal");
+        data.add("Usa");
+
+        val adapter = RvAdapter(data)
+
+        recyclerview.adapter = adapter
     }
+}
+
+private fun <E> ArrayList<E>.add(element: Int) {
+
 }
--- app/src/main/java/com/example/todoapplication/adapter/RvAdapter.kt
+++ app/src/main/java/com/example/todoapplication/adapter/RvAdapter.kt
@@ -1,4 +1,36 @@
 package com.example.todoapplication.adapter
 
-class RvAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.todoapplication.R
+import java.util.*
+
+class RvAdapter(private val dataSet: ArrayList<String>) : RecyclerView.Adapter<RvAdapter.ViewHolder>(){
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val textView: TextView
+
+        init {
+            // Define click listener for the ViewHolder's View.
+            textView = view.findViewById(R.id.rvdata)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.rvdata, parent, false)
+
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.textView.text = dataSet[position];
+    }
+
+    override fun getItemCount() = dataSet.size
+
 }
